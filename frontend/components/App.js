cd75ef85167b3dd3228b9c3687f067f43de1427b
Andy Le <andylequ@gmail.com>
2024-11-15 17:06:24 -0600

    version1

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -5,6 +5,7 @@ import LoginForm from './LoginForm'
 import Message from './Message'
 import ArticleForm from './ArticleForm'
 import Spinner from './Spinner'
+import axios from 'axios'
 
 const articlesUrl = 'http://localhost:9000/api/articles'
 const loginUrl = 'http://localhost:9000/api/login'
@@ -18,8 +19,8 @@ export default function App() {
 
   // ✨ Research `useNavigate` in React Router v.6
   const navigate = useNavigate()
-  const redirectToLogin = () => { /* ✨ implement */ }
-  const redirectToArticles = () => { /* ✨ implement */ }
+  const redirectToLogin = () => {navigate('/login')}
+  const redirectToArticles = () => {navigate('/articles')}
 
   const logout = () => {
     // ✨ implement
@@ -27,18 +28,47 @@ export default function App() {
     // and a message saying "Goodbye!" should be set in its proper state.
     // In any case, we should redirect the browser back to the login screen,
     // using the helper above.
+    localStorage.removeItem('token')
+    navigate('/')
   }
 
-  const login = ({ username, password }) => {
+  const login = async ({ username, password }) => {
     // ✨ implement
     // We should flush the message state, turn on the spinner
     // and launch a request to the proper endpoint.
     // On success, we should set the token to local storage in a 'token' key,
     // put the server success message in its proper state, and redirect
     // to the Articles screen. Don't forget to turn off the spinner!
+
+    setMessage('')
+    setSpinnerOn(true)
+
+    try {
+      const response = await fetch('http://localhost:9000/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',     
+      },
+        body: JSON.stringify({username, password})
+      });
+
+      if(!response.ok){
+        throw new Error('Login failed');
+      }
+      const data = await response.json();
+      const token = data.token;
+      localStorage.setItem('token', token)
+      setMessage('success')
+      setSpinnerOn(false)
+      navigate('/articles')
+    } catch (error) {
+      console.error(error)
+    }
+    
+    
   }
 
-  const getArticles = () => {
+  const getArticles = async () => {
     // ✨ implement
     // We should flush the message state, turn on the spinner
     // and launch an authenticated request to the proper endpoint.
@@ -47,6 +77,44 @@ export default function App() {
     // If something goes wrong, check the status of the response:
     // if it's a 401 the token might have gone bad, and we should redirect to login.
     // Don't forget to turn off the spinner!
+    setMessage('')
+    setSpinnerOn(true)
+
+    const token = localStorage.getItem('token')
+
+    if(!token){
+      console.error('No token found. Please login first.')
+      return;
+    }
+
+    try{
+      const response = await fetch(`http://localhost:9000/api/articles`, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      })
+
+      if(!response.ok){
+        navigate('/login')
+        throw new Error('Failed to fetch data')
+      }
+
+      //creating data from json response
+      const data = await response.json()
+
+      //setting articles
+      setArticles(data)
+
+      console.log('fetched data', data)
+
+      //turning off spinner
+      setSpinnerOn(false)
+
+    } catch (error) {
+      console.log(error)
+    }
+
   }
 
   const postArticle = article => {
